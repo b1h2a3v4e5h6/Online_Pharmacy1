7eb8332ba1c57a416e32856829e44731f7ce63f2
Bhaveshparwani <bhaveshparwani1373@gmail.com>
2024-03-07 13:19:48 +0530

    UPDATE Pharmacy\settings.py

--- Pharmacy/settings.py
+++ Pharmacy/settings.py
@@ -126,8 +126,13 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 LOGIN_REDIRECT_URL = '/'
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL=False
+EMAIL_HOST_USER = 'sv7144035@gmail.com'
+EMAIL_HOST_PASSWORD = 'tvrykpvhvnugviap'
 
 
 # Default primary key field type
